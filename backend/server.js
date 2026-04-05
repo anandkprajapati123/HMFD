9e3906566e012bcdce6d27a9f09ceeac72ddd9dc
anandkprajapati123 <anandkprajapati123@gmail.com>
2026-04-05 17:54:06 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
