7ea8024c3dc34b00dae3248b4af2546cd47c59e2
anandkprajapati123 <anandkprajapati123@gmail.com>
2026-04-05 20:39:16 +0530

    added folder

--- frontend/src/pages/Success/Success.jsx
+++ frontend/src/pages/Success/Success.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./success.css";
+import "./Success.css";
 
 const Success = () => {
   const navigate = useNavigate();
