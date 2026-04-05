61ef17d935e48a581ba6836cefcde7a55cc5891e
anandkprajapati123 <anandkprajapati123@gmail.com>
2026-04-05 18:42:17 +0530

    Update API URL to production endpoint

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -11,7 +11,7 @@ import axios from "axios";
 const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:5000";
+  const url = "https://food-delivery-back-gu3y.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
